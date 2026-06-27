9dadf2533ee7030714779c56e1cd61190c55cac9
Shivang Mishra <your-email@example.com>
2026-06-27 23:17:52 +0530

    Completed login and signup UI with basic validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,41 @@ import {
   Typography,
   TextField,
   Button,
-  
+
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Email } from "@mui/icons-material";
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const handleLogin = () => {
+
+    setEmailError("");
+    setPasswordError("");
+
+    let valid = true;
+
+    if (email === "") {
+      setEmailError("Email is required");
+      valid = false;
+    }
+
+    if (password === "") {
+      setPasswordError("Password is required");
+      valid = false;
+    }
+
+    if (valid) {
+      alert("Login Successful");
+    }
+
+  };
   return (
     <Box
       sx={{
@@ -40,6 +70,10 @@ const Login = () => {
           label="Email"
           fullWidth
           margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          error={emailError !== ""}
+          helperText={emailError}
         />
 
         <TextField
@@ -47,22 +81,30 @@ const Login = () => {
           type="password"
           fullWidth
           margin="normal"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          error={passwordError !== ""}
+          helperText={passwordError}
         />
 
         <Button
-          variant="contained"
-          fullWidth
-          sx={{mt: 3,py: 1.4,borderRadius: 2,
-            }}
-        >
-          Login
-        </Button>
+  variant="contained"
+  fullWidth
+  onClick={handleLogin}
+  sx={{
+    mt: 3,
+    py: 1.4,
+    borderRadius: 2,
+  }}
+>
+  Login
+</Button>
         <Typography textAlign="center" mt={2}>
-  Don't have an account?{" "}
-  <Link to="/signup" style={{ textDecoration: "none" }}>
-    Sign Up
-  </Link>
-</Typography>
+          Don't have an account?{" "}
+          <Link to="/signup" style={{ textDecoration: "none" }}>
+            Sign Up
+          </Link>
+        </Typography>
       </Paper>
     </Box>
   );
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -6,7 +6,61 @@ import {
   Button,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Signup = () => {
+  const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const [confirmPassword, setConfirmPassword] = useState("");
+
+const [nameError, setNameError] = useState("");
+const [emailError, setEmailError] = useState("");
+const [passwordError, setPasswordError] = useState("");
+const [confirmPasswordError, setConfirmPasswordError] = useState("");
+const handleSignup = () => {
+
+  setNameError("");
+  setEmailError("");
+  setPasswordError("");
+  setConfirmPasswordError("");
+
+  let valid = true;
+
+  if (name === "") {
+    setNameError("Name is required");
+    valid = false;
+  }
+
+  if (email === "") {
+    setEmailError("Email is required");
+    valid = false;
+  }
+if (email !== "" && !email.includes("@")) {
+  setEmailError("Enter a valid email");
+  valid = false;
+}
+  if (password === "") {
+    setPasswordError("Password is required");
+    valid = false;
+  }else if (password.length < 6) {
+  setPasswordError("Password must be at least 6 characters");
+  valid = false;
+}
+
+  if (confirmPassword === "") {
+    setConfirmPasswordError("Confirm Password is required");
+    valid = false;
+  }
+
+  if (password !== confirmPassword) {
+    setConfirmPasswordError("Passwords do not match");
+    valid = false;
+  }
+
+  if (valid) {
+    alert("Signup Successful");
+  }
+};
   return (
     <Box
       sx={{
@@ -31,47 +85,63 @@ const Signup = () => {
           fontWeight="bold"
           mb={3}
         >
-          Sign Up
+          SIGN UP
         </Typography>
 
         <TextField
-          label="Full Name"
-          fullWidth
-          margin="normal"
-        />
+  label="Full Name"
+  fullWidth
+  margin="normal"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+  error={nameError !== ""}
+  helperText={nameError}
+/>
 
         <TextField
-          label="Email"
-          type="email"
-          fullWidth
-          margin="normal"
-        />
+  label="Email"
+  type="email"
+  fullWidth
+  margin="normal"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  error={emailError !== ""}
+  helperText={emailError}
+/>
 
         <TextField
-          label="Password"
-          type="password"
-          fullWidth
-          margin="normal"
-        />
+  label="Password"
+  type="password"
+  fullWidth
+  margin="normal"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+  error={passwordError !== ""}
+  helperText={passwordError}
+/>
 
-        <TextField
-          label="Confirm Password"
-          type="password"
-          fullWidth
-          margin="normal"
-        />
-
-        <Button
-          variant="contained"
-          fullWidth
-          sx={{
-            mt: 3,
-            py: 1.5,
-            borderRadius: 2,
-          }}
-        >
-          Sign Up
-        </Button>
+      <TextField
+  label="Confirm Password"
+  type="password"
+  fullWidth
+  margin="normal"
+  value={confirmPassword}
+  onChange={(e) => setConfirmPassword(e.target.value)}
+  error={confirmPasswordError !== ""}
+  helperText={confirmPasswordError}
+/>
+ <Button
+  variant="contained"
+  fullWidth
+  onClick={handleSignup}
+  sx={{
+    mt: 3,
+    py: 1.5,
+    borderRadius: 2,
+  }}
+>
+  Sign Up
+</Button>
         <Typography textAlign="center" mt={2}>
   Already have an account?{" "}
   <Link to="/login" style={{ textDecoration: "none" }}>
